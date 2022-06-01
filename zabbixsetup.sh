33b2016e1c214b825f3f01cfba63b5f5d4c81fd7
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-06-01 22:12:44 +0700

    Update zabbixsetup.sh

--- zabbixsetup.sh
+++ zabbixsetup.sh
@@ -5,6 +5,7 @@ echo -e "\e[1m\e[32mInstall mysql-server ... \e[0m" && sleep 1
 apt-get update && apt-get upgrade -y
 apt-get install mysql-server -y
 systemctl start mysql.service
+sleep 3
 
 
 #1. Download the Zabbix and install
@@ -13,11 +14,13 @@ echo -e "\e[1m\e[32mDownload the Zabbix and install ... \e[0m" && sleep 1
 wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+ubuntu20.04_all.deb
 dpkg -i zabbix-release_6.0-1+ubuntu20.04_all.deb
 apt -y update
+sleep 3
 
 #2. Install Zabbix server, frontend, agent
 echo " "
 echo -e "\e[1m\e[32mInstall Zabbix server, frontend, agent ... \e[0m" && sleep 1
 apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+sleep 3
 
 #3. Create initial database.
 echo " "
@@ -25,29 +28,34 @@ echo -e "\e[1m\e[32mCreate initial database ... \e[0m" && sleep 1
 mysql -u root -e "create database zabbix character set utf8mb4 collate utf8mb4_bin"; 
 mysql -u root -e "create user zabbix@localhost identified by 'password'";
 mysql -u root -e  "grant all privileges on zabbix.* to zabbix@localhost";
+sleep 3
 
 
 #4. Install the zabbix database.
 echo " "
 echo -e "\e[1m\e[32mInstall the zabbix database. ... \e[0m" && sleep 1
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -u zabbix -ppassword --database=zabbix
+sleep 3
 
 
 #5. Configure the database for Zabbix server
 echo " "
 echo -e "\e[1m\e[32mConfigure the database for Zabbix server ... \e[0m" && sleep 1
 echo DBPassword=password >> /etc/zabbix/zabbix_server.conf
+sleep 3
 
 #6. Start Zabbix server and agent processes
 echo " "
 echo -e "\e[1m\e[32mDStart Zabbix server and agent processes ... \e[0m" && sleep 1
 systemctl restart zabbix-server zabbix-agent apache2
 systemctl enable zabbix-server zabbix-agent apache2
+sleep 3
 
 #7. Install zabbix-agent2
 echo " "
 echo -e "\e[1m\e[32mInstall zabbix-agent2 ... \e[0m" && sleep 1
 apt -y install zabbix-agent2
+sleep 3
 
 
 #8. Setup agent2 config
@@ -65,6 +73,7 @@ chmod 775  /var/run/docker.sock
 sudo usermod -aG docker zabbix
 sudo systemctl restart zabbix-agent2
 sudo systemctl enable zabbix-agent2
+sleep 3
 
 #9. Detail for add server on zabbix
 echo " "
